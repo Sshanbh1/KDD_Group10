c6fc392a36b5cd0eb8e0af144aabd22aa0bae415
pranavkamble3010 <pkamble@uncc.edu>
2019-10-29 11:43:09 -0400

    Merge

--- src/com/uncc/KDD/UserInterface.java
+++ src/com/uncc/KDD/UserInterface.java
@@ -21,10 +21,14 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
 
 public class UserInterface implements Observer{
 
-	private JFrame frame;
+	private JFrame frmKddProjectGroup;
 	private JTextArea textArea;
 	private File dataFile;
 	private File headerFile;
@@ -48,7 +52,7 @@ public class UserInterface implements Observer{
 			public void run() {
 				try {
 					UserInterface window = new UserInterface();
-					window.frame.setVisible(true);
+					window.frmKddProjectGroup.setVisible(true);
 					
 					UIManager.setLookAndFeel(
 				            UIManager.getSystemLookAndFeelClassName());
@@ -76,19 +80,65 @@ public class UserInterface implements Observer{
 		actionFinder = new Logic();
 		actionFinder.addObserver(this);
 		
-		frame = new JFrame();
-		frame.setBounds(100, 100, 603, 585);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmKddProjectGroup = new JFrame();
+		frmKddProjectGroup.setResizable(false);
+		frmKddProjectGroup.setTitle("KDD Project- Group 10");
+		frmKddProjectGroup.getContentPane().setBackground(new Color(105, 105, 105));
+		frmKddProjectGroup.setBounds(100, 100, 603, 585);
+		frmKddProjectGroup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmKddProjectGroup.getContentPane().setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 321, 585, 217);
-		frame.getContentPane().add(scrollPane);
+		frmKddProjectGroup.getContentPane().add(scrollPane);
 		
 		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setBackground(new Color(255, 255, 255));
 		scrollPane.setViewportView(textArea);
 		
-		JButton btnRun = new JButton("Execute");
+		JPanel panel = new JPanel();
+		panel.setBackground(UIManager.getColor("Button.background"));
+		panel.setBounds(10, 203, 563, 107);
+		frmKddProjectGroup.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblMinimumSupport = new JLabel("Min Support:");
+		lblMinimumSupport.setBounds(10, 14, 88, 14);
+		lblMinimumSupport.setHorizontalAlignment(SwingConstants.LEFT);
+		panel.add(lblMinimumSupport);
+		
+		minConfidenceTextField = new JTextField();
+		minConfidenceTextField.setBounds(108, 11, 96, 20);
+		panel.add(minConfidenceTextField);
+		minConfidenceTextField.setToolTipText("Enter as a percentage value");
+		minConfidenceTextField.setColumns(10);
+		
+		JLabel lblMinimumConfidence = new JLabel("Min Confidence:");
+		lblMinimumConfidence.setBounds(10, 49, 88, 14);
+		panel.add(lblMinimumConfidence);
+		
+		minSupportTextField = new JTextField();
+		minSupportTextField.setBounds(108, 46, 96, 20);
+		panel.add(minSupportTextField);
+		minSupportTextField.setColumns(10);
+		
+		JButton btnRun = new JButton("Run");
+		btnRun.setBounds(465, 71, 88, 25);
+		panel.add(btnRun);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(310, 11, 127, 85);
+		panel.add(scrollPane_1);
+		
+		stableAttributesList = new JList<String>();
+		stableAttributesList.setVisibleRowCount(10);
+		scrollPane_1.setViewportView(stableAttributesList);
+		
+		JLabel lblStableAttributes = new JLabel("Stable attributes:");
+		lblStableAttributes.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStableAttributes.setBounds(204, 11, 105, 20);
+		panel.add(lblStableAttributes);
 		btnRun.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				boolean correctInput = true;
@@ -134,127 +184,70 @@ public class UserInterface implements Observer{
 				}				
 			}
 		});
-		btnRun.setBounds(476, 283, 97, 25);
-		frame.getContentPane().add(btnRun);
 		
-		JButton btnChooseDataFile = new JButton("Select data file");
-		btnChooseDataFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser fileFind = new JFileChooser();
-				int returnVal = fileFind.showOpenDialog(null);
-
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					dataFile = fileFind.getSelectedFile();
-					dataFileField.setText(dataFile.getPath());
-				}	
-			}
-		});
-		btnChooseDataFile.setBounds(0, 0, 128, 25);
-		frame.getContentPane().add(btnChooseDataFile);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 130, 563, 62);
+		frmKddProjectGroup.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
 		
-		dataFileField = new JTextField();
-		dataFileField.setEditable(false);
-		dataFileField.setBounds(195, 1, 390, 22);
-		frame.getContentPane().add(dataFileField);
-		dataFileField.setColumns(10);
-		
-		JButton btnChooseAttributeName = new JButton("Select file with attribute names");
-		btnChooseAttributeName.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFileChooser fileFind = new JFileChooser();
-				int returnVal = fileFind.showOpenDialog(null);
-
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					headerFile = fileFind.getSelectedFile();
-					headerFileField.setText(headerFile.getPath());
-				}
-				headerFileField.setText(headerFile.getPath());
-			}
-		});
-		btnChooseAttributeName.setBounds(0, 26, 183, 25);
-		frame.getContentPane().add(btnChooseAttributeName);
-		
-		headerFileField = new JTextField();
-		headerFileField.setEditable(false);
-		headerFileField.setColumns(10);
-		headerFileField.setBounds(195, 27, 390, 22);
-		frame.getContentPane().add(headerFileField);
+		JLabel lblChooseDecisionAttribute = new JLabel("Choose decision attribute: ");
+		lblChooseDecisionAttribute.setBounds(10, 11, 153, 14);
+		lblChooseDecisionAttribute.setHorizontalAlignment(SwingConstants.LEFT);
+		panel_1.add(lblChooseDecisionAttribute);
 		
 		decisionAttributeComboBox = new JComboBox<String>();
-		decisionAttributeComboBox.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent arg0) {
-				if(arg0.getStateChange() == ItemEvent.SELECTED) {
-					dInitialValueComboBox.removeAllItems();
-					dToValueComboBox.removeAllItems();
-					
-					HashSet<String> distinctValues = actionFinder.getDistinctAttributeValues((String)arg0.getItem());
-					
-					for(String value : distinctValues) {
-						dInitialValueComboBox.addItem(value);
-						dToValueComboBox.addItem(value);
-					}
-					dInitialValueComboBox.setEnabled(true);
-					dToValueComboBox.setEnabled(true);
-				}else {
-					dInitialValueComboBox.setEnabled(false);
-					dToValueComboBox.setEnabled(false);
-				}
-			}
-		});
-		decisionAttributeComboBox.setBounds(0, 94, 153, 22);
-		frame.getContentPane().add(decisionAttributeComboBox);
+		decisionAttributeComboBox.setBackground(Color.WHITE);
+		decisionAttributeComboBox.setBounds(10, 26, 153, 22);
+		panel_1.add(decisionAttributeComboBox);
 		
-		JLabel lblChooseDecisionAttribute = new JLabel("Select decision attribute: ");
-		lblChooseDecisionAttribute.setBounds(0, 79, 153, 16);
-		frame.getContentPane().add(lblChooseDecisionAttribute);
-		
-		JLabel lblInitialValue = new JLabel("Initial-Value:");
-		lblInitialValue.setBounds(235, 79, 72, 16);
-		frame.getContentPane().add(lblInitialValue);
-		
-		JLabel lblEndValue = new JLabel("End-Value:");
-		lblEndValue.setBounds(420, 79, 62, 16);
-		frame.getContentPane().add(lblEndValue);
+		JLabel lblInitialValue = new JLabel("Initial Value:");
+		lblInitialValue.setBounds(173, 11, 88, 16);
+		panel_1.add(lblInitialValue);
 		
 		dInitialValueComboBox = new JComboBox<String>();
-		dInitialValueComboBox.setBounds(235, 94, 88, 22);
-		frame.getContentPane().add(dInitialValueComboBox);
+		dInitialValueComboBox.setBackground(Color.WHITE);
+		dInitialValueComboBox.setBounds(173, 26, 88, 22);
+		panel_1.add(dInitialValueComboBox);
+		
+		JLabel lblEndValue = new JLabel("End Value:");
+		lblEndValue.setBounds(271, 11, 109, 16);
+		panel_1.add(lblEndValue);
 		
 		dToValueComboBox = new JComboBox<String>();
-		dToValueComboBox.setBounds(420, 94, 88, 22);
-		frame.getContentPane().add(dToValueComboBox);
+		dToValueComboBox.setBackground(Color.WHITE);
+		dToValueComboBox.setBounds(271, 26, 109, 22);
+		panel_1.add(dToValueComboBox);
 		
-		JLabel lblStableAttributes = new JLabel("Stable-attributes:");
-		lblStableAttributes.setBounds(0, 148, 99, 16);
-		frame.getContentPane().add(lblStableAttributes);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 11, 563, 108);
+		frmKddProjectGroup.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(0, 166, 128, 142);
-		frame.getContentPane().add(scrollPane_1);
+		dataFileField = new JTextField();
+		dataFileField.setBounds(10, 11, 348, 22);
+		panel_2.add(dataFileField);
+		dataFileField.setBackground(Color.WHITE);
+		dataFileField.setEditable(false);
+		dataFileField.setColumns(10);
 		
-		stableAttributesList = new JList<String>();
-		scrollPane_1.setViewportView(stableAttributesList);
+		headerFileField = new JTextField();
+		headerFileField.setBounds(10, 40, 348, 22);
+		panel_2.add(headerFileField);
+		headerFileField.setBackground(Color.WHITE);
+		headerFileField.setEditable(false);
+		headerFileField.setColumns(10);
 		
-		JLabel lblMinimumSupport = new JLabel("Minimum-Support:");
-		lblMinimumSupport.setBounds(195, 148, 128, 16);
-		frame.getContentPane().add(lblMinimumSupport);
+		JButton btnChooseDataFile = new JButton("Choose data file");
+		btnChooseDataFile.setBounds(368, 10, 185, 25);
+		panel_2.add(btnChooseDataFile);
 		
-		JLabel lblMinimumConfidence = new JLabel("Minimum-Confidence:");
-		lblMinimumConfidence.setBounds(195, 205, 128, 16);
-		frame.getContentPane().add(lblMinimumConfidence);
+		JButton btnChooseAttributeName = new JButton("Choose attribute name file");
+		btnChooseAttributeName.setBounds(368, 39, 185, 25);
+		panel_2.add(btnChooseAttributeName);
 		
-		minSupportTextField = new JTextField();
-		minSupportTextField.setBounds(335, 145, 44, 22);
-		frame.getContentPane().add(minSupportTextField);
-		minSupportTextField.setColumns(10);
-		
-		minConfidenceTextField = new JTextField();
-		minConfidenceTextField.setToolTipText("Enter as a percentage value");
-		minConfidenceTextField.setColumns(10);
-		minConfidenceTextField.setBounds(335, 202, 44, 22);
-		frame.getContentPane().add(minConfidenceTextField);
-		
-		JButton btnLoadFiles = new JButton("Upload files...");
+		JButton btnLoadFiles = new JButton("Load files...");
+		btnLoadFiles.setBounds(10, 72, 109, 25);
+		panel_2.add(btnLoadFiles);
 		btnLoadFiles.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(!(dataFile == null) && !(headerFile == null)){
@@ -277,9 +270,50 @@ public class UserInterface implements Observer{
 				}
 			}
 		});
-		btnLoadFiles.setBounds(476, 47, 109, 25);
-		frame.getContentPane().add(btnLoadFiles);
-		frame.setVisible(true);
+		btnChooseAttributeName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileFind = new JFileChooser();
+				int returnVal = fileFind.showOpenDialog(null);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					headerFile = fileFind.getSelectedFile();
+					headerFileField.setText(headerFile.getPath());
+				}
+				headerFileField.setText(headerFile.getPath());
+			}
+		});
+		btnChooseDataFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileFind = new JFileChooser();
+				int returnVal = fileFind.showOpenDialog(null);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					dataFile = fileFind.getSelectedFile();
+					dataFileField.setText(dataFile.getPath());
+				}	
+			}
+		});
+		decisionAttributeComboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				if(arg0.getStateChange() == ItemEvent.SELECTED) {
+					dInitialValueComboBox.removeAllItems();
+					dToValueComboBox.removeAllItems();
+					
+					HashSet<String> distinctValues = actionFinder.getDistinctAttributeValues((String)arg0.getItem());
+					
+					for(String value : distinctValues) {
+						dInitialValueComboBox.addItem(value);
+						dToValueComboBox.addItem(value);
+					}
+					dInitialValueComboBox.setEnabled(true);
+					dToValueComboBox.setEnabled(true);
+				}else {
+					dInitialValueComboBox.setEnabled(false);
+					dToValueComboBox.setEnabled(false);
+				}
+			}
+		});
+		frmKddProjectGroup.setVisible(true);
 	}
 
 	/**
